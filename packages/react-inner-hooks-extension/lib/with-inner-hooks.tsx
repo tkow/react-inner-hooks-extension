0070281e1afa00af15d3f20b8bc7e37efd577da0
tkow <t.kusama0613@outlook.jp>
2022-02-12 20:59:06 +0900

    feat: with container displayname

--- packages/react-inner-hooks-extension/lib/with-inner-hooks.tsx
+++ packages/react-inner-hooks-extension/lib/with-inner-hooks.tsx
@@ -12,10 +12,10 @@ type WithInnerHooksReturnType<Props> = <
 export function withInnerHooks<Props extends Record<string, any>>(
   Child: React.ComponentType<Props>
 ): WithInnerHooksReturnType<Props> {
-  return <WithInnerHookProps extends RestProps<Props, IP>, IP extends Partial<Props> = {}>({
+  return function WithInnerHooksContainer<WithInnerHookProps extends RestProps<Props, IP>, IP extends Partial<Props> = {}>({
     innerHooks,
     ...props
-  }: WithInnerHookProps & { innerHooks?: () => IP }): ReactElement => {
+  }: WithInnerHookProps & { innerHooks?: () => IP }): ReactElement {
     const ex = (innerHooks && innerHooks()) as IP
     const composeProps = { ...props, ...ex } as Props
     return <Child {...composeProps} />
