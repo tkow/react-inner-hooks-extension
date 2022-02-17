57f37020d26c3a713fd3ccebde0b9749b34ad156
tkow <t.kusama0613@outlook.jp>
2022-02-17 18:41:05 +0900

    chore: sample type

--- packages/react-inner-hooks-extension/example/components/NumberInput.tsx
+++ packages/react-inner-hooks-extension/example/components/NumberInput.tsx
@@ -1,10 +1,10 @@
-import { ComponentType, forwardRef } from 'react'
+import { ComponentType, ForwardedRef, forwardRef } from 'react'
 import Input from './Input'
 import { withInnerHooks } from '../../'
 
 type ExtractProps<C> = C extends ComponentType<infer Props> ? Props : any
 
-function NumberInput(props: Omit<ExtractProps<typeof Input>, 'type'>, ref) {
+function NumberInput(props: Omit<ExtractProps<typeof Input>, 'type'>, ref: ForwardedRef<HTMLInputElement>) {
   return <Input type="number" {...props} ref={ref} />
 }
 
