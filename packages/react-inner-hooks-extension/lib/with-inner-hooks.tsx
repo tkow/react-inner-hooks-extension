338c14d8f13e59242207724455e83f42500d5d9a
tkow <t.kusama0613@outlook.jp>
2022-03-02 21:29:11 +0900

    fix: allow void

--- packages/react-inner-hooks-extension/lib/with-inner-hooks.tsx
+++ packages/react-inner-hooks-extension/lib/with-inner-hooks.tsx
@@ -1,12 +1,12 @@
 import React, { ComponentType, ForwardedRef, forwardRef, ReactElement, RefAttributes, MutableRefObject } from 'react'
 
-type RestProps<Props, PatialProps> = Omit<Props, keyof PatialProps>
+type RestProps<Props, PatialProps extends Partial<Props> | void> = Omit<Props,  PatialProps extends void ? never : keyof PatialProps>
 
-type ConnectContaierProps<Props, IP, RefValue = any> = {
+type ConnectContaierProps<Props, IP extends Partial<Props> | void, RefValue = any> = {
   connectContainer?: (props: RestProps<Props, IP>, ref?: MutableRefObject<RefValue>) => IP
 }
 
-type WithInnerHooksReturnType<Props> = <IP extends Partial<Props> = {}>(
+type WithInnerHooksReturnType<Props extends Record<string, any>> = <IP extends Partial<Props> | void = {}>(
   props: RestProps<Props, IP> & ConnectContaierProps<Props, IP>
 ) => ReactElement
 
@@ -20,11 +20,11 @@ export const CONTAINER_ID = 'WithInnerHooksContainer'
 export function withInnerHooks<Props extends Record<string, any>, Ref = any>(
   Child: ComponentType<Props>
 ): WithInnerHooksReturnType<Props & RefAttributes<Ref>> {
-  function WithInnerHooksContainer<IP extends Partial<Props> = {}>(
+  function WithInnerHooksContainer<IP extends Partial<Props> | void = {}>(
     { connectContainer, ...props }: RestProps<Props, IP> & ConnectContaierProps<Props, IP>,
     ref: ForwardedRef<Ref>
   ): ReactElement {
-    const ex = connectContainer && connectContainer(props as RestProps<Props, IP>, refCheck(ref))
+    const ex = connectContainer && connectContainer(props as unknown as RestProps<Props, IP>, refCheck<Ref>(ref))
     const composeProps = { ...props, ...ex } as Props
     return <Child ref={ref} {...composeProps} />
   }
--- packages/react-inner-hooks-extension/react-inner-hooks-extension.d.ts
+++ packages/react-inner-hooks-extension/react-inner-hooks-extension.d.ts
@@ -1,30 +1,31 @@
 import 'react'
 
 declare module 'react' {
-    export type RestProps<Props, PatialProps> = Omit<Props, keyof PatialProps>
+  export type RestProps<Props, PatialProps> = Omit<Props, keyof PatialProps>
 
-    export type AddInnerHooksProps<Props, IP = {}, RefValue = any> = RestProps<Props, ForceIP<IP>> & {
-        connectContainer?: (props: RestProps<Props, ForceIP<IP>>, ref?: MutableRefObject<RefValue>) => ForceIP<IP>
-    }
+  export type AddInnerHooksProps<Props, IP extends Partial<Props> | void = {}, RefValue = any> = RestProps<Props, ForceIP<IP>> & {
+    connectContainer?: (props: RestProps<Props, ForceIP<IP>>, ref?: MutableRefObject<RefValue>) => ForceIP<IP>
+  }
 
-    export type ForceIP<IP> = IP extends Exclude<any, IP> ? IP : {}
+  export type ForceIP<IP> = IP extends Exclude<any, IP> ?
+    IP : void
 
 
-    export interface VoidFunctionComponent<P = {}> {
-      <IP = {}, RefValue = any>(props: AddInnerHooksProps<P, IP, RefValue>, context?: any): ReactElement<any, any> | null;
-    }
-    export interface FunctionComponent<P = {}> {
-      <IP = {}, RefValue = any>(props: PropsWithChildren<AddInnerHooksProps<P, IP, RefValue>>, context?: any): ReactElement<any, any> | null;
-    }
-    export interface ComponentClass<P = {}, S = ComponentState> extends StaticLifecycle<P, S> {
-        new <IP = {}, RefValue = any>(props: AddInnerHooksProps<P, IP, RefValue>, context?: any): Component<AddInnerHooksProps<P, IP, RefValue>, S>;
-    }
-    export interface ExoticComponent<P = {}> {
-        /**
-         * **NOTE**: Exotic components are not callable.
-         */
-         <IP = {}, RefValue = any>(props: AddInnerHooksProps<P, IP, RefValue extends unknown ? any : RefValue>): (ReactElement|null);
-    }
+  export interface VoidFunctionComponent<P = {}> {
+    <IP = {}, RefValue = any>(props: AddInnerHooksProps<P, IP, RefValue>, context?: any): ReactElement<any, any> | null;
+  }
+  export interface FunctionComponent<P = {}> {
+    <IP = {}, RefValue = any>(props: PropsWithChildren<AddInnerHooksProps<P, IP, RefValue>>, context?: any): ReactElement<any, any> | null;
+  }
+  export interface ComponentClass<P = {}, S = ComponentState> extends StaticLifecycle<P, S> {
+    new <IP = {}, RefValue = any>(props: AddInnerHooksProps<P, IP, RefValue>, context?: any): Component<AddInnerHooksProps<P, IP, RefValue>, S>;
+  }
+  export interface ExoticComponent<P = {}> {
+    /**
+     * **NOTE**: Exotic components are not callable.
+     */
+    <IP = {}, RefValue = any>(props: AddInnerHooksProps<P, IP, RefValue extends unknown ? any : RefValue>): (ReactElement | null);
+  }
 
 }
 
